119b77f48f1b03b651f07c5581eeec8ba0814fe7
Camila Fernandes <camila-fernandes-d>
2026-05-29 23:42:36 -0300

    minhas alterações 2.0

--- models/atendimento.py
+++ models/atendimento.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 class Atendimento:
 
-    def __init__(self):
+    def __init__(self, cliente, atendente):
         self.cliente = cliente
         self.atendente = atendente
         self.data_inicio = datetime.now()
